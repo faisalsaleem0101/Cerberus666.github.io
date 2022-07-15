9712cfd64d0f35d6354dbb1a47a00879a3ccac64
Faisal Saleem <faisalsaleem0101@gmail.com>
2022-07-15 12:25:48 +0500

    website

---
+++ _next/static/UMZTGIg3pk0OCHkYD8aP9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-0870b93e6bf1580f0fdc.js"],"/_error":["static/chunks/pages/_error-077790e38fbfe3891eb2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
